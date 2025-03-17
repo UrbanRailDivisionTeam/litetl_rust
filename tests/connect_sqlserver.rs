665337893d9366e2af82d4212742065517ee086e
pitlst <1442450941@qq.com>
2025-03-18 00:14:19 +0800

    mongo连接测试完成

--- tests/connect_sqlserver.rs
+++ tests/connect_sqlserver.rs
@@ -1,3 +1,9 @@
+use tiberius::Client;
+use tiberius::Config;
+
+static CONN_STR: &str = "server=tcp:127.0.0.1\\SQL2022D;IntegratedSecurity=true;TrustServerCertificate=true";
+
+#[test]
 fn main(){
     println!("hello word");
 }
