1e6c2dc7ab20a4d4d7fdd59cc695b85645b72eaf
cheakf <sunmoming#gmail.com>
2025-03-17 21:40:31 +0800

    测试

--- tests/connect_pgsql.rs
+++ tests/connect_pgsql.rs
@@ -3,7 +3,7 @@ use postgres::NoTls;
 
 #[test]
 fn main(){
-    let mut client = Client::connect("postgresql://postgres:Swq8855830.@localhost", NoTls).unwrap();
+    let mut client = Client::connect("postgresql://postgres:Swq8855830.@172.30.240.39", NoTls).unwrap();
     let rows = client.query("SELECT 1", &[]).unwrap();
     if !rows.is_empty() {
         println!("pgsql连接成功");
--- tests/connect_sqlserver.rs
+++ tests/connect_sqlserver.rs
@@ -1,3 +1,10 @@
-fn main(){
-    println!("hello word");
+use futures::stream::{self, StreamExt};
+use tiberius::{Client, Config, AuthMethod};
+use tokio::net::TcpStream;
+
+#[test] 
+async fn main(){
+    // 提取数据
+    let source_data = extract().await?;
+
 }
